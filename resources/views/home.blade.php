3bb5a1305b2c5e4ad2c4a1d2a4183abe74ada597
Salva Ros <1916778@alu.murciaeduca.es>
2024-11-14 09:55:02 +0100

    cambios dopetrope

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Mi Web</title>
+    </head>
+    <body>
+        <h1>¡Hola <?php echo $nombre ?? 'colegon'; ?>!</h1>
+        <p>La hora actual es {{ now()}}.</p>
+        
+    </body>
+</html>
