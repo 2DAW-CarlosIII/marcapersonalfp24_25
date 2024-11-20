c4155f5c3f047da3defb2f940152480d05086fb6
alumno <659304@alu.murciaeduca.es>
2024-11-20 09:10:25 +0100

    Ejercicios de controladores resuelto o casi

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Route::get('/', [HomeController::class, 'getHome']);
 
 Route::get('login', function() {
     return view('auth.login');
@@ -14,22 +16,16 @@ Route::get('logout', function() {
     return 'Logout usuario';
 });
 
-Route::get('proyectos', function() {
-    return view('proyectos.index');
-});
+Route::get('proyectos', [ProyectosController::class, 'getIndex']);
 
-Route::get('proyectos/show/{id}', function($id) {
-    return view('proyectos.show', array('id'=>$id));
-})->where('id', '[0-9]+');
+Route::get('proyectos/show/{id}', [ProyectosController::class, 'getShow'])->where('id', '[0-9]+');
 
-Route::get('proyectos/create', function() {
-    return view('proyectos.create');
-});
+Route::get('proyectos/create', [ProyectosController::class, 'getCreate']);
 
-Route::get('proyectos/edit/{id}', function($id) {
-    return view('proyectos.edit', array('id'=>$id));
-})->where('id', '[0-9]+');
+Route::get('proyectos/edit/{id}', [ProyectosController::class, 'getEdit'])->where('id', '[0-9]+');
 
 Route::get('perfil/{id?}', function($id = null) {
     return $id ? 'Visualizar el currículo de '. $id : 'Visualizar el currículo propio';
 })->where('id', '[0-9]*');
+
+Route::get('user/{nombre}', [UserController::class, 'showProfile']);
