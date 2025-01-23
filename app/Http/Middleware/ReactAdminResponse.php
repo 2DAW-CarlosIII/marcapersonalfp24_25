f65323d5806cc1038418a1d3fdb4d680c9f36087
Pablo Moran <4684887@alu.murciaeduca.es>
2025-01-23 09:46:15 +0100

    El parametro q no funciona en Middleware

--- app/Http/Middleware/ReactAdminResponse.php
+++ app/Http/Middleware/ReactAdminResponse.php
@@ -26,16 +26,44 @@ class ReactAdminResponse
         }
         $response = $next($request);
         if ($request->routeIs('*.index')) {
-            abort_unless(property_exists($request->route()->controller, 'modelclass'), 500, "It must exists a modelclass property in the controller.");
-            $modelClassName = $request->route()->controller->modelclass;
-            $response->header('X-Total-Count', $modelClassName::count());
+
+            //abort_unless(property_exists($request->route()->controller, 'modelclass'), 500, "It must exists a modelclass property in the controller.");
+           // $response->header('X-Total-Count', $modelClassName::count());
         }
         try {
-            if (is_callable([$response, 'getData'])) {
+            /*if (is_callable([$response, 'getData'])) {
                 $responseData = $response->getData();
                 if (isset($responseData->data)) {
                     $response->setData($responseData->data);
                 }
+            }*/
+            $modelClassName = $request->route()->controller->modelclass;
+
+            $sort = $request->_sort;
+            $order = $request->_order;
+            $q = $request->q;
+            $start = $request->_start;
+            $end = $request->_end;
+            $query = $modelClassName::query();
+            $fillable = $modelClassName::getFillable();
+
+            if($q){
+                $query->orWhere(function($query) use ($fillable, $q){
+                    foreach ($fillable as $key) {
+                        $query->orWhere($key, 'like', "%$q%");
+                    }
+                    return $query;
+                });
+            }
+
+            if($order && $sort){
+                $query->orderBy($sort, $order);
+            }
+            if($start){
+                $query->skip($start);
+            }
+            if($end){
+                $query->take($end - $start);
             }
         } catch (\Throwable $th) {
         }
